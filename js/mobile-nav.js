c483ecf07c17e35f2aaf3a22e9460804f4df4243
Sung Woo Min <sm2598@cornell.edu>
2018-12-05 21:13:41 -0500

    JS additions

---
+++ js/mobile-nav.js
@@ -0,0 +1,9 @@
+function mobileNavToggle(x) {
+    x.classList.toggle("change");
+  }
+
+$(document).ready(function() {
+  $("#mobileNavToggle").click(function() {
+  $("#mobile-nav-content").toggleClass("hidden");
+  });
+});
